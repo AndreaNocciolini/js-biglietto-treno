befd0e843cda678eb5d5967f26b8541bb01a3b33
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-19 16:34:23 +0100

    minor-fixes

--- js/script.js
+++ js/script.js
@@ -21,18 +21,25 @@ let underageDiscount = parseFloat((ticketPriceFix * 20) / 100);
 let underageDiscountFix = underageDiscount.toFixed(2);
 console.log(`Underage's Discount(<18): ` + underageDiscountFix + `€`);
 
+let underagePrice = ticketPriceFix - underageDiscountFix;
+let underagePriceFix = underagePrice.toFixed(2);
+console.log(`Underage's ticket price: ` + underagePriceFix + `€`)
+
 // va applicato uno sconto del 40% per gli over 65.
 
 let overageDiscount = parseFloat((ticketPriceFix * 40) / 100);
 let overageDiscountFix = overageDiscount.toFixed(2);
 console.log(`Elder's Discount(>65): ` + overageDiscountFix + `€`);
 
+let overagePrice = ticketPriceFix - overageDiscountFix;
+let overagePriceFix = underagePrice.toFixed(2);
+console.log(`Overage's ticket price: ` + overagePriceFix + `€`)
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
 if (age < 18) {
-    document.getElementById(`my_div`).innerHTML = `The cost of the ticket will be <span class="price">${ticketPriceFix - underageDiscountFix}</span> €.`;
+    document.getElementById(`my_div`).innerHTML = `The cost of the ticket will be <span class="price">${underagePriceFix}</span> €.`;
 } else if (age > 65) {
-    document.getElementById(`my_div`).innerHTML = `The cost of the ticket will be <span class="price">${ticketPriceFix - overageDiscountFix}</span> €.`;
+    document.getElementById(`my_div`).innerHTML = `The cost of the ticket will be <span class="price">${overagePriceFix}</span> €.`;
 } else {
     document.getElementById(`my_div`).innerHTML = `The cost of the ticket will be <span class="price">${ticketPriceFix}</span> €.`
 }
